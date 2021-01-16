72f48de59288a69a3ef60f4673a74da9b6e9536e
GManners-Trinity <gmanners@trinity.edu>
2021-01-15 19:41:22 -0600

    initial setup for GavBot 2.0

---
+++ bot.py
@@ -0,0 +1,35 @@
+import os
+
+import asyncio
+import discord
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+amOnline = False
+
+@client.event
+async def on_ready():
+    global amOnline
+    amOnline = True
+    print('Logged in as {0.user}'.format(client))
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$quit'):
+        global amOnline
+        amOnline = False
+            
+        print("Shutting down")
+        await message.channel.send('Shutting down!')
+        exit(1) 
+
+
+client.run(TOKEN)
