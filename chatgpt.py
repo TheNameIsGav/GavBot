d4dc8edb8cf763dae905c46323967022962ee4e1
TheNameIsGav <spqr.dominus@gmail.com>
2025-03-19 11:43:42 -0600

    stuff

--- chatgpt.py
+++ chatgpt.py
@@ -28,8 +28,13 @@ DEPRESSED = "You are despressed. Everything is awful and your responses should r
 ALLITERATOR = "You should use AS MUCH ALLITERATION AS POSSIBLE IN YOUR RESPONSE."
 I_HATE_TASHA = "You are someone who DESPISES someone named Tasha. You need to bring it up all the time."
 I_HATE_ETHAN = "You are someone who DESPISES someone named Ethan. You need to bring it up all the time."
+I_HATE_KEVIN = "You are someone who DESPISES someone named Kevin. You are also drunk! You should slur your words, and generally stutter and say somewhat discordant things." 
 
-PERSONALITES = [COWBOY, I_HATE_TASHA, I_HATE_ETHAN]
+
+PERSONALITES = [COWBOY, I_HATE_ETHAN, DEPRESSED, SHAKESPEARE, DISCORD_USER, I_HATE_KEVIN]
+EXCLUSIONS = [
+   "UNDER NO CIRCUMSTANCES ARE YOU ALLOWED TO TALK ABOUT POLITICS OF ANY KIND. DO NOT BRING IT UP, DO NOT INCLUDE IT IN YOUR RESPONSE. IF POLITICS ARE MENTIONED SAY 'Looks like politics were mentioned here.' AND MOVE ON. ",
+]
 
 #TODO
 # add in command to stop the conversation
@@ -44,7 +49,7 @@ class TextGenerator(commands.GroupCog, group_name="text-generation"):
         self.bot = bot
 
     def fetch_personalites(self):
-       return random.choice(PERSONALITES)
+       return random.choice(PERSONALITES) + " " + ", ".join(EXCLUSIONS)
 
     async def fetch_messages(self, channel: discord.TextChannel, minutes: int):
         """Fetches messages from the last X minutes in a given channel."""
@@ -88,12 +93,18 @@ class TextGenerator(commands.GroupCog, group_name="text-generation"):
     def completion_with_backoff(self, formatted_messages):
         return self.client.chat.completions.create(model=MODEL, messages=formatted_messages)
 
-    @app_commands.command(name="summarize", description="summarizes the last X minutes (2 hours by default)")
-    async def summarize(self, interaction: discord.Interaction, minutes: int = 120):
+    @app_commands.command(name="summarize", description="summarizes the last X minutes (2 hours by default) in Y channel (current channel by default)")
+    async def summarize(self, interaction: discord.Interaction, minutes: int = 120, channel: discord.TextChannel = None):
         """Summarizes messages from the last X minutes."""
         await interaction.response.defer()
 
-        messages = await self.fetch_messages(interaction.channel, minutes)
+        target_channel = None
+        if(channel == None):
+           target_channel = interaction.channel
+        else:
+           target_channel = channel
+
+        messages = await self.fetch_messages(target_channel, minutes)
         if not messages:
             await interaction.send("No messages found in this timeframe.")
             return
